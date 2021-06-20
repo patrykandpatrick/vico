a93d4e3c785b47bc4fec153b1eabd9efb4fb3b24
Patryk <patryk.goworowski@oke.pl>
2021-06-20 09:37:06 +0200

    Add `SpannableExtensions` files with two extension functions

---
+++ liftchart-common/src/main/java/pl/patrykgoworowski/liftchart_common/extension/SpannableExtensions.kt
@@ -0,0 +1,40 @@
+package pl.patrykgoworowski.liftchart_common.extension
+
+import android.os.Build
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+
+public fun SpannableStringBuilder.appendCompat(
+    text: CharSequence,
+    what: Any,
+    flags: Int,
+) =
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        append(text, what, flags)
+    } else {
+        append(text, 0, text.length)
+        setSpan(what, length - text.length, length, flags)
+        this
+    }
+
+public fun <T> Iterable<T>.transformToSpannable(
+    separator: CharSequence = ", ",
+    prefix: CharSequence = "",
+    postfix: CharSequence = "",
+    limit: Int = -1,
+    truncated: CharSequence = "…",
+    transform: SpannableStringBuilder.(T) -> Unit,
+): Spannable {
+    val buffer = SpannableStringBuilder()
+    buffer.append(prefix)
+    var count = 0
+    for (element in this) {
+        if (++count > 1) buffer.append(separator)
+        if (limit < 0 || count <= limit) {
+            buffer.transform(element)
+        } else break
+    }
+    if (limit in 0 until count) buffer.append(truncated)
+    buffer.append(postfix)
+    return buffer
+}
