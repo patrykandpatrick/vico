1678f0054ed9e5625d3f2f80393c6de0777992ce
Patryk <patryk.goworowski@oke.pl>
2021-07-10 17:54:37 +0200

    Add function allowing copying and editing individual color channels for color Int

---
+++ liftchart-common/src/main/java/pl/patrykgoworowski/liftchart_common/extension/ColorExtensions.kt
@@ -0,0 +1,20 @@
+package pl.patrykgoworowski.liftchart_common.extension
+
+private const val ALPHA_BIT_SHIFT = 24
+private const val RED_BIT_SHIFT = 16
+private const val GREEN_BIT_SHIFT = 8
+private const val BLUE_BIT_SHIFT = 0
+
+fun Int.copyColor(
+    alpha: Int = this.extractColorChannel(ALPHA_BIT_SHIFT),
+    red: Int = this.extractColorChannel(RED_BIT_SHIFT),
+    green: Int = this.extractColorChannel(GREEN_BIT_SHIFT),
+    blue: Int = this.extractColorChannel(BLUE_BIT_SHIFT),
+): Int = (alpha shl ALPHA_BIT_SHIFT) or
+        (red shl RED_BIT_SHIFT) or
+        (green shl GREEN_BIT_SHIFT) or
+        (blue shl BLUE_BIT_SHIFT)
+
+
+private fun Int.extractColorChannel(bitShift: Int): Int =
+    this shr bitShift and 0xff
