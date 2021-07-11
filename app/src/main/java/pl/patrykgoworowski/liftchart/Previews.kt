4e7eeccd6ded77349cc382b19a51c9ecc372dfc8
Patryk <patryk.goworowski@oke.pl>
2021-07-11 11:32:10 +0200

    Update previews

--- app/src/main/java/pl/patrykgoworowski/liftchart/Previews.kt
+++ app/src/main/java/pl/patrykgoworowski/liftchart/Previews.kt
@@ -10,32 +10,37 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import pl.patrykgoworowski.liftchart.ui.MainTheme
 import pl.patrykgoworowski.liftchart_common.axis.AxisManager
 import pl.patrykgoworowski.liftchart_common.axis.VerticalAxis
 import pl.patrykgoworowski.liftchart_common.axis.component.GuidelineComponent
-import pl.patrykgoworowski.liftchart_common.component.RectComponent
 import pl.patrykgoworowski.liftchart_common.data_set.entry.collection.single.SingleEntryList
 import pl.patrykgoworowski.liftchart_common.entry.entriesOf
-import pl.patrykgoworowski.liftchart_common.extension.dp
+import pl.patrykgoworowski.liftchart_compose.component.dimension.setPadding
+import pl.patrykgoworowski.liftchart_compose.component.rectComponent
 import pl.patrykgoworowski.liftchart_compose.component.shapeComponent
 import pl.patrykgoworowski.liftchart_compose.component.textComponent
 import pl.patrykgoworowski.liftchart_compose.data_set.bar.ColumnChart
+import pl.patrykgoworowski.liftchart_compose.extension.pixels
 import pl.patrykgoworowski.liftchart_compose.path.chartShape
 
 @Preview("Sample DataSet", widthDp = 200)
 @Composable
-fun SampleDataSet(modifier: Modifier = Modifier) {
+fun SampleDataSet(modifier: Modifier = Modifier) = MainTheme {
+    val colors = MaterialTheme.colors
     ColumnChart(
         modifier = modifier,
         singleEntryCollection = SingleEntryList(entriesOf(1 to 1, 2 to 2, 3 to 3, 4 to 2)),
         axisManager = AxisManager(
             startAxis = VerticalAxis.start(
                 label = textComponent(
-                    color = MaterialTheme.colors.primary,
+                    color = colors.primary,
                     textSize = 10f.sp,
                     background = shapeComponent(
                         shape = CutCornerShape(
@@ -44,30 +49,28 @@ fun SampleDataSet(modifier: Modifier = Modifier) {
                             CornerSize(50),
                             CornerSize(25)
                         ),
-                        color = MaterialTheme.colors.primary.copy(0.1f),
+                        color = colors.primary.copy(0.1f),
                     )
                 ).apply {
-                    padding.apply {
-                        end = 8f.dp
-                        start = 4f.dp
-                    }
+                    setPadding(end = 8f.dp, start = 4f.dp)
                 },
                 axis = null,
                 tick = null,
                 guideline = GuidelineComponent(
-                    MaterialTheme.colors.primary.copy(0.1f).toArgb(),
-                    1f.dp
+                    colors.primary.copy(0.1f).toArgb(),
+                    1f.dp.pixels,
                 ),
             ),
             topAxis = null,
             endAxis = null,
             bottomAxis = null
         ),
-        column = RectComponent(
-            MaterialTheme.colors.primary.toArgb(),
+        column = rectComponent(
+            colors.primary,
             thickness = 8f.dp,
-            shape = CutCornerShape(Dp(4f)).chartShape()
-        )
+            shape = RoundedCornerShape(Dp(4f)).chartShape()
+        ),
+        columnBrush = Brush.verticalGradient(listOf(colors.primary, colors.secondary))
     )
 }
 
