6df0b0f6f754a0d8bc76c6438791b9fd3290c2ea
Patryk Michalik <contact@patrykmichalik.com>
2021-08-24 21:33:29 +0200

    Reformat & optimize `TickComponent`

--- liftchart-common/src/main/java/pl/patrykgoworowski/liftchart_common/axis/component/TickComponent.kt
+++ liftchart-common/src/main/java/pl/patrykgoworowski/liftchart_common/axis/component/TickComponent.kt
@@ -5,11 +5,10 @@ import pl.patrykgoworowski.liftchart_common.component.shape.shader.DynamicShader
 import pl.patrykgoworowski.liftchart_common.path.Shape
 import pl.patrykgoworowski.liftchart_common.path.rectShape
 
-
-public open class TickComponent(
+open class TickComponent(
     color: Int,
     thickness: Float,
-    public var length: Float = 2 * thickness,
+    var length: Float = 2 * thickness,
     shape: Shape = rectShape(),
     dynamicShader: DynamicShader? = null
 ) : LineComponent(color, thickness, shape, dynamicShader)
