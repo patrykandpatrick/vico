73049d6b84b8b908796fdb4588b34f97925a43ef
Patryk Goworowski <patryk.goworowski@oke.pl>
2021-09-12 14:04:35 +0200

    Move `Map` extensions from `CollectionExtensions` to `MapExtensions`

---
+++ vico-core/src/main/java/pl/patrykgoworowski/vico/core/extension/MapExtensions.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021. Patryk Goworowski
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package pl.patrykgoworowski.vico.core.extension
+
+import android.graphics.PointF
+import pl.patrykgoworowski.vico.core.marker.Marker
+import java.util.TreeMap
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
+fun Map<Float, List<Marker.EntryModel>>.getClosestMarkerEntryPositionModel(
+    touchPoint: PointF,
+): List<Marker.EntryModel>? =
+    keys.findClosestPositiveValue(touchPoint.x)
+        ?.let(::get)
+
+public fun <K, V> TreeMap<K, MutableList<V>>.updateAll(other: Map<K, List<V>>) {
+    other.forEach { (key, value) ->
+        put(key, get(key)?.apply { addAll(value) } ?: mutableListOf(value))
+    }
+}
+
+internal inline fun <K, V> HashMap<K, MutableList<V>>.updateList(
+    key: K,
+    initialCapacity: Int = 0,
+    block: MutableList<V>.() -> Unit,
+) {
+    block(getOrPut(key) { ArrayList(initialCapacity) })
+}
