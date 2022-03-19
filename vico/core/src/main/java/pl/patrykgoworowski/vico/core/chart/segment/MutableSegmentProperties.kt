4c0f930ab628e09a1c308e9f7e8291ddf8a98b4f
Patryk Goworowski <patrykgoworowski@gmail.com>
2022-03-19 16:14:56 +0100

    Add documentation for `MutableSegmentProperties.kt`

--- vico/core/src/main/java/pl/patrykgoworowski/vico/core/chart/segment/MutableSegmentProperties.kt
+++ vico/core/src/main/java/pl/patrykgoworowski/vico/core/chart/segment/MutableSegmentProperties.kt
@@ -16,6 +16,9 @@
 
 package pl.patrykgoworowski.vico.core.chart.segment
 
+/**
+ * The implementation of [SegmentProperties] which has all of its properties mutable.
+ */
 public data class MutableSegmentProperties(
     override var cellWidth: Float = 0f,
     override var marginWidth: Float = 0f,
