bcd1e8523730f2133736f874af0c1c67613c1012
Patryk <contact@patrykmichalik.com>
2022-03-19 22:06:35 +0100

    Add documentation for `DEF_LABEL_LINE_COUNT`, `DEF_LABEL_COUNT`, and `DEF_LABEL_SPACING`

--- vico/core/src/main/java/pl/patrykgoworowski/vico/core/Defaults.kt
+++ vico/core/src/main/java/pl/patrykgoworowski/vico/core/Defaults.kt
@@ -19,8 +19,19 @@ package pl.patrykgoworowski.vico.core
 import pl.patrykgoworowski.vico.core.component.shape.DashedShape
 import pl.patrykgoworowski.vico.core.component.text.TextComponent
 
+/**
+ * The default line count for [TextComponent].
+ */
 public const val DEF_LABEL_LINE_COUNT: Int = 1
+
+/**
+ * The default maximum label count for vertical axes.
+ */
 public const val DEF_LABEL_COUNT: Int = 99
+
+/**
+ * The default label spacing for vertical axes in dp.
+ */
 public const val DEF_LABEL_SPACING: Float = 16f
 
 /**
