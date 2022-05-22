439e13ec2995727ace1a52dd22c3db6633309bc7
Patryk Michalik <contact@patrykmichalik.com>
2022-05-22 16:45:47 +0200

    Finalize `ViewLineChart`

--- sample/src/main/java/com/patrykandpatryk/vico/sample/chart/views/ViewLineChart.kt
+++ sample/src/main/java/com/patrykandpatryk/vico/sample/chart/views/ViewLineChart.kt
@@ -19,8 +19,10 @@ package com.patrykandpatryk.vico.sample.chart.views
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidViewBinding
+import com.patrykandpatryk.vico.core.axis.Axis
 import com.patrykandpatryk.vico.core.entry.ChartEntryModelProducer
-import com.patrykandpatryk.vico.databinding.BasicLineChartBinding
+import com.patrykandpatryk.vico.databinding.LineChartBinding
+import com.patrykandpatryk.vico.sample.util.SampleChartTokens
 import com.patrykandpatryk.vico.sample.util.marker
 
 @Composable
@@ -29,11 +31,17 @@ internal fun ViewLineChart(
     modifier: Modifier = Modifier,
 ) {
     val marker = marker()
+    val tokens = SampleChartTokens.LineChart
     AndroidViewBinding(
-        factory = BasicLineChartBinding::inflate,
+        factory = LineChartBinding::inflate,
         modifier = modifier,
     ) {
         chart.entryProducer = chartEntryModelProducer
         chart.marker = marker
+        (chart.bottomAxis as Axis).guideline = null
+        chart.chart?.addPersistentMarker(
+            x = tokens.PERSISTENT_MARKER_X,
+            marker = marker,
+        )
     }
 }
