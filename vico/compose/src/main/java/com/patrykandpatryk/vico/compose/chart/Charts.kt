d73cdb640df21658972e2a20e411088b393eeb88
Patryk Michalik <contact@patrykmichalik.com>
2022-05-26 09:58:03 +0200

    Correct inconsistency in documentation

--- vico/compose/src/main/java/com/patrykandpatryk/vico/compose/chart/Charts.kt
+++ vico/compose/src/main/java/com/patrykandpatryk/vico/compose/chart/Charts.kt
@@ -134,7 +134,7 @@ public fun <Model : ChartEntryModel> Chart(
  * @param bottomAxis an axis displayed on the bottom of the chart.
  * @param marker an optional marker that will appear when the chart is touched, highlighting the entry or entries
  * nearest to the touch point.
- * @param legend an optional legend for this chart.
+ * @param legend an optional legend for the chart.
  * @param isHorizontalScrollEnabled whether horizontal scroll is enabled.
  * @param isZoomEnabled whether zooming in and out is enabled.
  */
