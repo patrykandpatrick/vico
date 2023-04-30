a39721d8c7e221ad59e5e32286cf14b120e297ca
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2023-04-30 14:25:30 +0000

    Update dependencies

--- vico/compose/src/main/java/com/patrykandpatrick/vico/compose/extension/PointerInputScopeExtensions.kt
+++ vico/compose/src/main/java/com/patrykandpatrick/vico/compose/extension/PointerInputScopeExtensions.kt
@@ -13,46 +13,44 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Modifications copyright 2022 by Patryk Goworowski and Patrick Michalik.
+ * Modifications copyright 2023 by Patryk Goworowski and Patrick Michalik.
  */
 
 package com.patrykandpatrick.vico.compose.extension
 
+import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.calculateCentroid
 import androidx.compose.foundation.gestures.calculateCentroidSize
 import androidx.compose.foundation.gestures.calculateZoom
-import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.positionChanged
 import kotlin.math.abs
 
 internal suspend fun PointerInputScope.detectZoomGestures(onGesture: (centroid: Offset, zoom: Float) -> Unit) {
-    forEachGesture {
-        awaitPointerEventScope {
-            var zoom = 1f
-            var pastTouchSlop = false
-            val touchSlop = viewConfiguration.touchSlop
-            awaitFirstDown(requireUnconsumed = false)
-            do {
-                val event = awaitPointerEvent()
-                val canceled = event.changes.any { it.isConsumed }
-                if (!canceled) {
-                    val zoomChange = event.calculateZoom()
-                    if (!pastTouchSlop) {
-                        zoom *= zoomChange
-                        val centroidSize = event.calculateCentroidSize(useCurrent = false)
-                        val zoomMotion = abs(1 - zoom) * centroidSize
-                        if (zoomMotion > touchSlop) pastTouchSlop = true
-                    }
-                    if (pastTouchSlop) {
-                        val centroid = event.calculateCentroid(useCurrent = false)
-                        if (zoomChange != 1f) onGesture(centroid, zoomChange)
-                        event.changes.forEach { if (it.positionChanged()) it.consume() }
-                    }
+    awaitEachGesture {
+        var zoom = 1f
+        var pastTouchSlop = false
+        val touchSlop = viewConfiguration.touchSlop
+        awaitFirstDown(requireUnconsumed = false)
+        do {
+            val event = awaitPointerEvent()
+            val canceled = event.changes.any { it.isConsumed }
+            if (!canceled) {
+                val zoomChange = event.calculateZoom()
+                if (!pastTouchSlop) {
+                    zoom *= zoomChange
+                    val centroidSize = event.calculateCentroidSize(useCurrent = false)
+                    val zoomMotion = abs(1 - zoom) * centroidSize
+                    if (zoomMotion > touchSlop) pastTouchSlop = true
                 }
-            } while (!canceled && event.changes.any { it.pressed })
-        }
+                if (pastTouchSlop) {
+                    val centroid = event.calculateCentroid(useCurrent = false)
+                    if (zoomChange != 1f) onGesture(centroid, zoomChange)
+                    event.changes.forEach { if (it.positionChanged()) it.consume() }
+                }
+            }
+        } while (!canceled && event.changes.any { it.pressed })
     }
 }
