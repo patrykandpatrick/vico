1d872439177db7ed3914e94e67246548f72106e8
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2024-02-19 22:20:56 +0100

    Make `rememberMutableMeasureContext` internal

--- vico/compose/src/main/java/com/patrykandpatrick/vico/compose/layout/MeasureContextExtensions.kt
+++ vico/compose/src/main/java/com/patrykandpatrick/vico/compose/layout/MeasureContextExtensions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 by Patryk Goworowski and Patrick Michalik.
+ * Copyright 2024 by Patryk Goworowski and Patrick Michalik.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,23 +22,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.LayoutDirection
-import com.patrykandpatrick.vico.core.chart.CartesianChart
 import com.patrykandpatrick.vico.core.chart.layout.HorizontalLayout
 import com.patrykandpatrick.vico.core.chart.values.ChartValues
 import com.patrykandpatrick.vico.core.context.MutableMeasureContext
-import com.patrykandpatrick.vico.core.model.CartesianChartModel
 
-/**
- * Creates and remembers a [MutableMeasureContext] instance.
- *
- * @param isHorizontalScrollEnabled whether horizontal scrolling is enabled.
- * @param canvasBounds the bounds of the canvas that will be used to draw the chart and its components.
- * @param horizontalLayout defines how the chart’s content is positioned horizontally.
- * @param spToPx converts dimensions from sp to px.
- * @param chartValues houses the [CartesianChart]’s [CartesianChartModel] and _x_ and _y_ ranges.
- */
 @Composable
-public fun rememberMutableMeasureContext(
+internal fun rememberMutableMeasureContext(
     isHorizontalScrollEnabled: Boolean,
     canvasBounds: RectF,
     horizontalLayout: HorizontalLayout,
