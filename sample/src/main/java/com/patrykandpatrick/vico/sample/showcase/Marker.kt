130231c9c2fba5c804f0ab0ebb94935c67c8ebe6
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2024-02-27 18:04:03 +0100

    Update `rememberMarker`
    Co-authored-by: Patryk Goworowski <patrykgoworowski@gmail.com>

--- sample/src/main/java/com/patrykandpatrick/vico/sample/showcase/Marker.kt
+++ sample/src/main/java/com/patrykandpatrick/vico/sample/showcase/Marker.kt
@@ -20,47 +20,43 @@ import android.graphics.Typeface
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import com.patrykandpatrick.vico.compose.component.rememberLineComponent
 import com.patrykandpatrick.vico.compose.component.rememberOverlayingComponent
 import com.patrykandpatrick.vico.compose.component.rememberShapeComponent
 import com.patrykandpatrick.vico.compose.component.rememberTextComponent
+import com.patrykandpatrick.vico.compose.component.shape.dashedShape
+import com.patrykandpatrick.vico.compose.component.shape.markerCorneredShape
 import com.patrykandpatrick.vico.compose.dimensions.dimensionsOf
 import com.patrykandpatrick.vico.core.chart.dimensions.HorizontalDimensions
 import com.patrykandpatrick.vico.core.chart.insets.Insets
 import com.patrykandpatrick.vico.core.component.marker.MarkerComponent
-import com.patrykandpatrick.vico.core.component.shape.DashedShape
-import com.patrykandpatrick.vico.core.component.shape.ShapeComponent
 import com.patrykandpatrick.vico.core.component.shape.Shapes
 import com.patrykandpatrick.vico.core.component.shape.cornered.Corner
-import com.patrykandpatrick.vico.core.component.shape.cornered.MarkerCorneredShape
 import com.patrykandpatrick.vico.core.context.MeasureContext
 import com.patrykandpatrick.vico.core.extension.copyColor
 import com.patrykandpatrick.vico.core.marker.Marker
 
 @Composable
 internal fun rememberMarker(labelPosition: MarkerComponent.LabelPosition = MarkerComponent.LabelPosition.Top): Marker {
-    val labelBackgroundColor = MaterialTheme.colorScheme.surface
+    val labelBackgroundShape = Shapes.markerCorneredShape(Corner.FullyRounded)
     val labelBackground =
-        remember(labelBackgroundColor) {
-            ShapeComponent(labelBackgroundShape, labelBackgroundColor.toArgb()).setShadow(
-                radius = LABEL_BACKGROUND_SHADOW_RADIUS,
-                dy = LABEL_BACKGROUND_SHADOW_DY,
+        rememberShapeComponent(labelBackgroundShape, MaterialTheme.colorScheme.surface)
+            .setShadow(
+                radius = LABEL_BACKGROUND_SHADOW_RADIUS_DP,
+                dy = LABEL_BACKGROUND_SHADOW_DY_DP,
                 applyElevationOverlay = true,
             )
-        }
     val label =
         rememberTextComponent(
+            color = MaterialTheme.colorScheme.onSurface,
             background = labelBackground,
-            lineCount = LABEL_LINE_COUNT,
-            padding = labelPadding,
+            padding = dimensionsOf(8.dp, 4.dp),
             typeface = Typeface.MONOSPACE,
         )
     val indicatorInnerComponent = rememberShapeComponent(Shapes.pillShape, MaterialTheme.colorScheme.surface)
-    val indicatorCenterComponent = rememberShapeComponent(Shapes.pillShape, Color.White)
-    val indicatorOuterComponent = rememberShapeComponent(Shapes.pillShape, Color.White)
+    val indicatorCenterComponent = rememberShapeComponent(Shapes.pillShape)
+    val indicatorOuterComponent = rememberShapeComponent(Shapes.pillShape)
     val indicator =
         rememberOverlayingComponent(
             outer = indicatorOuterComponent,
@@ -68,25 +64,25 @@ internal fun rememberMarker(labelPosition: MarkerComponent.LabelPosition = Marke
                 rememberOverlayingComponent(
                     outer = indicatorCenterComponent,
                     inner = indicatorInnerComponent,
-                    innerPaddingAll = indicatorInnerAndCenterComponentPaddingValue,
+                    innerPaddingAll = 5.dp,
                 ),
-            innerPaddingAll = indicatorCenterAndOuterComponentPaddingValue,
+            innerPaddingAll = 10.dp,
         )
     val guideline =
         rememberLineComponent(
-            MaterialTheme.colorScheme.onSurface.copy(GUIDELINE_ALPHA),
-            guidelineThickness,
-            guidelineShape,
+            color = MaterialTheme.colorScheme.onSurface.copy(.2f),
+            thickness = 2.dp,
+            shape = Shapes.dashedShape(shape = Shapes.pillShape, dashLength = 8.dp, gapLength = 4.dp),
         )
     return remember(label, labelPosition, indicator, guideline) {
         object : MarkerComponent(label, labelPosition, indicator, guideline) {
             init {
-                indicatorSizeDp = INDICATOR_SIZE_DP
+                indicatorSizeDp = 36f
                 onApplyEntryColor = { entryColor ->
-                    indicatorOuterComponent.color = entryColor.copyColor(INDICATOR_OUTER_COMPONENT_ALPHA)
+                    indicatorOuterComponent.color = entryColor.copyColor(alpha = .15f)
                     with(indicatorCenterComponent) {
                         color = entryColor
-                        setShadow(radius = INDICATOR_CENTER_COMPONENT_SHADOW_RADIUS, color = entryColor)
+                        setShadow(radius = 12f, color = entryColor)
                     }
                 }
             }
@@ -98,7 +94,11 @@ internal fun rememberMarker(labelPosition: MarkerComponent.LabelPosition = Marke
             ) {
                 with(context) {
                     outInsets.top =
-                        (SHADOW_RADIUS_MULTIPLIER * LABEL_BACKGROUND_SHADOW_RADIUS - LABEL_BACKGROUND_SHADOW_DY).pixels
+                        (
+                            CLIPPING_FREE_SHADOW_RADIUS_MULTIPLIER * LABEL_BACKGROUND_SHADOW_RADIUS_DP -
+                                LABEL_BACKGROUND_SHADOW_DY_DP
+                        )
+                            .pixels
                     if (labelPosition == LabelPosition.AroundPoint) return
                     outInsets.top += label.getHeight(context) + labelBackgroundShape.tickSizeDp.pixels
                 }
@@ -107,22 +107,6 @@ internal fun rememberMarker(labelPosition: MarkerComponent.LabelPosition = Marke
     }
 }
 
-private const val LABEL_BACKGROUND_SHADOW_RADIUS = 4f
-private const val LABEL_BACKGROUND_SHADOW_DY = 2f
-private const val LABEL_LINE_COUNT = 1
-private const val GUIDELINE_ALPHA = .2f
-private const val INDICATOR_SIZE_DP = 36f
-private const val INDICATOR_OUTER_COMPONENT_ALPHA = 32
-private const val INDICATOR_CENTER_COMPONENT_SHADOW_RADIUS = 12f
-private const val GUIDELINE_DASH_LENGTH_DP = 8f
-private const val GUIDELINE_GAP_LENGTH_DP = 4f
-private const val SHADOW_RADIUS_MULTIPLIER = 1.3f
-
-private val labelBackgroundShape = MarkerCorneredShape(Corner.FullyRounded)
-private val labelHorizontalPaddingValue = 8.dp
-private val labelVerticalPaddingValue = 4.dp
-private val labelPadding = dimensionsOf(labelHorizontalPaddingValue, labelVerticalPaddingValue)
-private val indicatorInnerAndCenterComponentPaddingValue = 5.dp
-private val indicatorCenterAndOuterComponentPaddingValue = 10.dp
-private val guidelineThickness = 2.dp
-private val guidelineShape = DashedShape(Shapes.pillShape, GUIDELINE_DASH_LENGTH_DP, GUIDELINE_GAP_LENGTH_DP)
+private const val LABEL_BACKGROUND_SHADOW_RADIUS_DP = 4f
+private const val LABEL_BACKGROUND_SHADOW_DY_DP = 2f
+private const val CLIPPING_FREE_SHADOW_RADIUS_MULTIPLIER = 1.4f
