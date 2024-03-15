38cda3d39cfdb1fb529ecb421991abe0671d3740
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2024-03-15 18:59:39 +0100

    Improve `DefaultMarkerLabelFormatter`

--- vico/core/src/main/java/com/patrykandpatrick/vico/core/marker/DefaultMarkerLabelFormatter.kt
+++ vico/core/src/main/java/com/patrykandpatrick/vico/core/marker/DefaultMarkerLabelFormatter.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 by Patryk Goworowski and Patrick Michalik.
+ * Copyright 2024 by Patryk Goworowski and Patrick Michalik.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,35 +25,46 @@ import com.patrykandpatrick.vico.core.extension.transformToSpannable
 import com.patrykandpatrick.vico.core.model.CartesianLayerModel
 import com.patrykandpatrick.vico.core.model.ColumnCartesianLayerModel
 import com.patrykandpatrick.vico.core.model.LineCartesianLayerModel
+import java.text.DecimalFormat
 
 /**
- * The default label formatter used for markers. [colorCode] specifies whether to color-code the _y_ values in the
- * labels.
- *
- * @see MarkerLabelFormatter
+ * The default [MarkerLabelFormatter]. The _y_ values are formatted via [decimalFormat] and, if [colorCode] is `true`,
+ * color-coded.
  */
-public class DefaultMarkerLabelFormatter(private val colorCode: Boolean = true) : MarkerLabelFormatter {
+public open class DefaultMarkerLabelFormatter(
+    private val decimalFormat: DecimalFormat = defaultDecimalFormat,
+    private val colorCode: Boolean = true,
+) : MarkerLabelFormatter {
+    /** The default [MarkerLabelFormatter]. [colorCode] specifies whether to color-code the _y_ values. */
+    @Deprecated(
+        "Use the primary constructor, which has a `decimalFormat` parameter. (If you’re using named arguments, " +
+            "ignore this warning. The deprecated constructor is more specific, but the primary one matches and will " +
+            "be used once the deprecated one has been removed.)",
+    )
+    public constructor(colorCode: Boolean) : this(defaultDecimalFormat, colorCode)
+
     override fun getLabel(
         markedEntries: List<Marker.EntryModel>,
         chartValues: ChartValues,
     ): CharSequence =
         markedEntries.transformToSpannable(
-            prefix = if (markedEntries.size > 1) PATTERN.format(markedEntries.sumOf { it.entry.y }) + " (" else "",
+            prefix =
+                if (markedEntries.size > 1) decimalFormat.format(markedEntries.sumOf { it.entry.y }) + " (" else "",
             postfix = if (markedEntries.size > 1) ")" else "",
             separator = "; ",
         ) { model ->
             if (colorCode) {
                 appendCompat(
-                    PATTERN.format(model.entry.y),
+                    decimalFormat.format(model.entry.y),
                     ForegroundColorSpan(model.color),
                     Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
                 )
             } else {
-                append(PATTERN.format(model.entry.y))
+                append(decimalFormat.format(model.entry.y))
             }
         }
 
-    private val CartesianLayerModel.Entry.y
+    protected val CartesianLayerModel.Entry.y: Float
         get() =
             when (this) {
                 is ColumnCartesianLayerModel.Entry -> y
@@ -61,7 +72,13 @@ public class DefaultMarkerLabelFormatter(private val colorCode: Boolean = true) 
                 else -> throw IllegalArgumentException("Unexpected `CartesianLayerModel.Entry` implementation.")
             }
 
+    override fun equals(other: Any?): Boolean =
+        this === other ||
+            other is DefaultMarkerLabelFormatter && decimalFormat == other.decimalFormat && colorCode == other.colorCode
+
+    override fun hashCode(): Int = 31 * decimalFormat.hashCode() + colorCode.hashCode()
+
     private companion object {
-        const val PATTERN = "%.02f"
+        val defaultDecimalFormat = DecimalFormat("#.##;−#.##")
     }
 }
