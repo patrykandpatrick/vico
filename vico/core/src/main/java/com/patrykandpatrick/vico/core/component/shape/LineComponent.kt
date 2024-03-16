3740c0305ce8e816957c40ef2b8617f1ceec2069
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2024-03-16 14:20:06 +0100

    `LineComponent`: Update default value of `thicknessDp`

--- vico/core/src/main/java/com/patrykandpatrick/vico/core/component/shape/LineComponent.kt
+++ vico/core/src/main/java/com/patrykandpatrick/vico/core/component/shape/LineComponent.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 by Patryk Goworowski and Patrick Michalik.
+ * Copyright 2024 by Patryk Goworowski and Patrick Michalik.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ import com.patrykandpatrick.vico.core.dimensions.emptyDimensions
  */
 public open class LineComponent(
     color: Int,
-    public var thicknessDp: Float = 2f,
+    public var thicknessDp: Float = 1f,
     shape: Shape = rectShape,
     dynamicShader: DynamicShader? = null,
     margins: Dimensions = emptyDimensions(),
