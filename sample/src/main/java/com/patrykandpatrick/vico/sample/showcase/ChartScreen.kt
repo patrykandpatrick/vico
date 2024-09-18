325d4c6c3c23e6bafd7be96d2064a100567d10e6
Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>
2024-09-18 15:01:17 +0200

    Update dependencies

--- sample/src/main/java/com/patrykandpatrick/vico/sample/showcase/ChartScreen.kt
+++ sample/src/main/java/com/patrykandpatrick/vico/sample/showcase/ChartScreen.kt
@@ -37,10 +37,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
