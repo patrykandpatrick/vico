9666a8feff2a2b13b494b743b6e741956f4025f3
Patryk Goworowski <patrykgoworowski@gmail.com>
2025-03-09 10:28:00 +0100

    Adapt arrows' and citation's color to the theme in the sample app
    Co-authored-by: Patrick Michalik <120058021+patrickmichalik@users.noreply.github.com>

--- sample/app/src/commonMain/kotlin/com/patrykandpatrick/vico/sample/ChartScreen.kt
+++ sample/app/src/commonMain/kotlin/com/patrykandpatrick/vico/sample/ChartScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -86,10 +87,18 @@ internal fun ChartScreen(navController: NavController, initialChartID: Int, uiFr
         Alignment.CenterVertically,
       ) {
         IconButton(onClick = { chartID-- }, enabled = chartID > 0) {
-          Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+          Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onBackground,
+          )
         }
         IconButton(onClick = { chartID++ }, enabled = chartID < charts.lastIndex) {
-          Icon(imageVector = Icons.AutoMirrored.Filled.ArrowForward, contentDescription = null)
+          Icon(
+            imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onBackground,
+          )
         }
       }
     },
@@ -114,6 +123,7 @@ internal fun ChartScreen(navController: NavController, initialChartID: Int, uiFr
           modifier =
             Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
               .align(Alignment.BottomStart),
+          color = MaterialTheme.colorScheme.onBackground,
           fontSize = 12.sp,
         )
       }
