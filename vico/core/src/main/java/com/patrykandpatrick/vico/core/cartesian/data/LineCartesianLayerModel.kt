d8b9589c852515d9ea2f09f5ef31d68af2d8bc48
Jakub Ledwon <leedwon12@gmail.com>
2025-07-28 10:18:29 +0200

    fixup! Add seriesIndex to CartesianLayerModel.Entry to make indices easier to access

--- vico/core/src/main/java/com/patrykandpatrick/vico/core/cartesian/data/LineCartesianLayerModel.kt
+++ vico/core/src/main/java/com/patrykandpatrick/vico/core/cartesian/data/LineCartesianLayerModel.kt
@@ -155,8 +155,8 @@ public class LineCartesianLayerModel : CartesianLayerModel {
      * have the same size.
      */
     public fun series(x: Collection<Number>, y: Collection<Number>) {
-      val nextSeriesIndex = series.size
-      series.add(x.zip(y) { x, y -> Entry(x, y, nextSeriesIndex) })
+      val seriesIndex = series.size
+      series.add(x.zip(y) { x, y -> Entry(x, y, seriesIndex) })
     }
 
     /** Adds a series with the provided _y_ values ([y]), using their indices as the _x_ values. */
